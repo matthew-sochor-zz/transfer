c0912723411a7444faa62cfaaeab398f9058067c
Matthew Sochor <matthew.sochor@gmail.com>
2017-12-29 21:15:29 -0500

    Update to version 0.28 to fix zoom out range

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="transfer",
-    version="0.27",
+    version="0.28",
     description="Transfer learning for deep image classification",
     packages=find_packages(),
 

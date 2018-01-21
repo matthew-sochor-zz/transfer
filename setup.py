eb72b460a69c551e00721227c1404ba9be29486d
Matthew Sochor <matthew.sochor@gmail.com>
2018-01-21 09:18:51 -0500

    Update to v0.35 which should be platform independent

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="transfer",
-    version="0.34",
+    version="0.35",
     description="Transfer learning for deep image classification",
     packages=find_packages(),
 

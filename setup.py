5f5ba5f4aaab07b1d115aa3f1c69aa8499da798d
Matthew Sochor <matthew.sochor@gmail.com>
2018-02-13 21:08:52 -0500

    Update to version 0.37 to fix training

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="transfer",
-    version="0.36",
+    version="0.37",
     description="Transfer learning for deep image classification",
     packages=find_packages(),
 
